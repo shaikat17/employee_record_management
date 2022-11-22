58a67232271bfe745b5817ef637b37be2f7b7303
shaikat17 <shaikatpal56@gmail.com>
2022-11-22 13:16:35 +0600

    Employee Record Management Project Using Django

---
+++ employee/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
