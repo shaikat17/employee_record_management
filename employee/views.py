9a1102f9ea6b38dc0fd66f160cb02081029558c1
shaikat17 <shaikatpal56@gmail.com>
2022-11-22 16:19:04 +0600

    Working with registration model

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
@@ -8,4 +8,23 @@ def index(request):
 
 
 def registration(request):
+    error = ''
+
+    if request.method == 'POST':
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        em = request.POST['email']
+        ec = request.POST['emid']
+        pwd = request.POST['pwd']
+
+        try:
+            User.objects.create_user(first_name=fn, last_name=ln, username=em, password=pwd)
+            EmployeeDetail.objects.create(empcode=ec)
+
+            error = 'No'
+        except:
+            error = 'Yes'
     return render(request, 'registration.html')
+
+def emp_login(request):
+    return render('emp_login', emp_login, name='emp_login.html')
