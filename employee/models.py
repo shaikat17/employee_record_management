2b08235675bcb22b1c34d0c54a372280771d85b3
shaikat17 <shaikatpal56@gmail.com>
2022-11-23 13:04:16 +0600

    working with employee Home

--- employee/models.py
+++ employee/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class EmployeeDetails(models.Model):
+class EmployeeDetail(models.Model):
 
-    user = models.ForeignKey
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    empcode = models.CharField(max_length=50)
+    empdept = models.CharField(max_length=100, null=True)
+    designation = models.CharField(max_length=100, null=True)
+    contact = models.CharField(max_length=100, null=True)
+    gender = models.CharField(max_length=100, null=True)
+    joindate = models.DateField(null=True)
 
+    def __str__(self):
+        return self.user.username
--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
 
@@ -8,4 +9,38 @@ def index(request):
 
 
 def registration(request):
-    return render(request, 'registration.html')
+    error = ''
+
+    if request.method == 'POST':
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        em = request.POST['email']
+        ec = request.POST['emid']
+        pwd = request.POST['psw']
+
+        try:
+            user = User.objects.create_user(first_name=fn, last_name=ln, username=em, password=pwd)
+            EmployeeDetail.objects.create(user=user, empcode=ec)
+
+            error = 'No'
+        except:
+            error = 'Yes'
+    return render(request, 'registration.html', locals())
+
+def emp_login(request):
+    error = ''
+    if request.method == 'POST':
+        uName = request.POST['email']
+        uPass = request.POST['psw']
+
+        user = authenticate(username=uName, password=uPass)
+
+        if user:
+            login(request, user)
+            error = 'No'
+        else:
+            error = 'Yes'
+    return render(request, 'emp_login.html', locals())
+
+def emp_home(request):
+    return render(request, 'emp_home.html')
